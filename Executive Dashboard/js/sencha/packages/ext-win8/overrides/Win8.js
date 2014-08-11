adada337d8d56e9391714862fac41bf26b88f9f9
Arthur Kay <arthurakay@gmail.com>
2014-08-11 07:44:57 -0500

    Functioning Win8 app!

---
+++ Executive Dashboard/js/sencha/packages/ext-win8/overrides/Win8.js
@@ -0,0 +1,29 @@
+﻿//@ override Ext.env.Ready
+Ext.elevateFunction = function (fn, scope, args) {
+    //to satisfy the Windows 8 CSP:
+    // http://msdn.microsoft.com/en-us/library/windows/apps/hh767331....
+    return MSApp.execUnsafeLocalFunction(function () {
+        return fn.apply(scope, args);
+    });
+};
+
+//When microloader has finished loading all dependencies
+(function (Ready) {
+    var invokeAll = Ready.invokeAll;
+    Ready.invokeAll = function () {
+        return MSApp.execUnsafeLocalFunction(function () {
+            return invokeAll.call(Ready);
+        });
+    };
+})(Ext.env.Ready);
+
+//the Grids throw errors after loading their JSON data... callbacks need to be wrapped in elevateFunction()
+(function (E) {
+    var MScallback = E.callback;
+
+    E.callback = function (callback, scope, args, delay, caller, defaultScope) {
+        return MSApp.execUnsafeLocalFunction(function () {
+            return MScallback.call(E, callback, scope, args, delay, caller, defaultScope);
+        });
+    };
+})(Ext);
