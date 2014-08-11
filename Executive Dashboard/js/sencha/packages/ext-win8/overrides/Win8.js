7989801809eaf973d639ed9a2827da92443f2c56
Arthur Kay <arthurakay@gmail.com>
2014-08-11 14:09:03 -0500

    Updating the override bits

--- Executive Dashboard/js/sencha/packages/ext-win8/overrides/Win8.js
+++ Executive Dashboard/js/sencha/packages/ext-win8/overrides/Win8.js
@@ -1,4 +1,39 @@
 ﻿//@ override Ext.env.Ready
+(function (E) {
+    var v = E.getVersion();
+
+    if (v.major < 5 || (v.major === 5 && v.minor === 0 && v.patch === 0)) {
+        //<debug>
+        E.Error.raise('The "ext-win8" package must be used with Ext JS 5.0.1 or higher.');
+        //</debug>
+        return;
+    }
+
+    else if (v.major === 5 && v.minor === 0 && v.patch < 2) {
+        /*
+         * EXTJS-14614 is fixed in Ext JS > 5.0.1
+         */
+        (function (Ready) {
+            var invokeAll = Ready.invokeAll;
+            Ready.invokeAll = function () {
+                return MSApp.execUnsafeLocalFunction(function () {
+                    return invokeAll.call(Ready);
+                });
+            };
+        })(E.env.Ready);
+
+        (function (EXT) {
+            var MScallback = EXT.callback;
+
+            EXT.callback = function (callback, scope, args, delay, caller, defaultScope) {
+                return MSApp.execUnsafeLocalFunction(function () {
+                    return MScallback.call(EXT, callback, scope, args, delay, caller, defaultScope);
+                });
+            };
+        })(E);
+    }
+})(Ext);
+
 Ext.elevateFunction = function (fn, scope, args) {
     //to satisfy the Windows 8 CSP:
     // http://msdn.microsoft.com/en-us/library/windows/apps/hh767331....
@@ -6,24 +41,3 @@ Ext.elevateFunction = function (fn, scope, args) {
         return fn.apply(scope, args);
     });
 };
-
-//When microloader has finished loading all dependencies
-(function (Ready) {
-    var invokeAll = Ready.invokeAll;
-    Ready.invokeAll = function () {
-        return MSApp.execUnsafeLocalFunction(function () {
-            return invokeAll.call(Ready);
-        });
-    };
-})(Ext.env.Ready);
-
-//the Grids throw errors after loading their JSON data... callbacks need to be wrapped in elevateFunction()
-(function (E) {
-    var MScallback = E.callback;
-
-    E.callback = function (callback, scope, args, delay, caller, defaultScope) {
-        return MSApp.execUnsafeLocalFunction(function () {
-            return MScallback.call(E, callback, scope, args, delay, caller, defaultScope);
-        });
-    };
-})(Ext);
